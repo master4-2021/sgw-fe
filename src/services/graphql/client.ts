ac951af2444b9db333d50c195c94a5e9d0bd32b9
hiep <hiepdh1@vmogroup.com>
2024-06-26 04:21:41 +0700

    fix wrong backend env name

--- src/services/graphql/client.ts
+++ src/services/graphql/client.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: import.meta.env.BASE_URL,
+  uri: import.meta.env.VITE_API_BASE_URL,
   cache: new InMemoryCache(),
 });
 
