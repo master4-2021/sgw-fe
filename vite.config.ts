3671c5bf2c53ee07a40ea553c33b897d8d5b6b3c
hiep <hiepdh1@vmogroup.com>
2024-06-26 06:17:25 +0700

    add proxy

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      "/graphql": {
+        target: "http://sgw-alb-641860864.ap-southeast-1.elb.amazonaws.com",
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/graphql/, ""),
+      },
+    },
+  },
+});
